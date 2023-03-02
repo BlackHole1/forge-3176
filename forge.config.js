cba4d3a83704cef2b4b8c023ffe718dd768127e1
Black-Hole1 <158blackhole@gmail.com>
2023-03-02 12:03:58 +0800

    init

---
+++ forge.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  packagerConfig: {
+    name: "你好"
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: "你好"
+      },
+    },
+  ],
+};
